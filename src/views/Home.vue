ab2d2048f4301ab0eb06ae6ccc31b1c240cf8f81
RUBÉN MALDONADO <romancalli@hotmail.com>
2020-06-02 09:08:59 -0400

    Creation of table and the filter with imput

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,123 @@
+/* eslint-disable vue/valid-template-root */
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <label>Biblioteca Fulanito</label>
+    <br />
+    <select v-model="selectedOption">
+      <option>All</option>
+      <option>Books</option>
+      <option>Magazines</option>
+      <option>Newspapers</option>
+      <option>Others</option>
+    </select>
+    <br />
+    <input v-model="name" placeholder="escriba su nombre" />
+    <br />
+    <div :key="item.name + '1'" v-for="item in list">
+      <table>
+        <tr>
+          <td>
+            <span>{{ item.code }}</span>
+          </td>
+          <td>
+            <span>{{ item.name }}</span>
+          </td>
+          <td>
+            <span>{{ item.description }}</span>
+          </td>
+          <td>
+            <span>{{ item.category }}</span>
+          </td>
+        </tr>
+      </table>
+    </div>
+
+    <div :key="item.name" v-for="item in filterList">
+      <span>{{ item.name }}</span>
+      <br />
+      <span>{{ item.description }}</span>
+      <br />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    //Nothing
+  },
+
+  data() {
+    return {
+      name: "",
+      list: [
+        {
+          code: "B1",
+          name: "book1",
+          description: "This is the book1",
+          category: "Books"
+        },
+        {
+          code: "M1",
+          name: "Magazine1",
+          description: "This is the Magazine1",
+          category: "Magazines"
+        },
+        {
+          code: "N1",
+          name: "Newpaper1",
+          description: "This is the newpaper1",
+          category: "Newspapers"
+        },
+        {
+          code: "O1",
+          name: "other1",
+          description: "This is the other1",
+          category: "Others"
+        },
+        {
+          code: "B2",
+          name: "book2",
+          description: "This is the book2",
+          category: "Books"
+        },
+        {
+          code: "M2",
+          name: "Magazine2",
+          description: "This is the Magazine2",
+          category: "Magazines"
+        },
+        {
+          code: "N2",
+          name: "Newpaper2",
+          description: "This is the newpaper2",
+          category: "Newspapers"
+        },
+        {
+          code: "O2",
+          name: "other2",
+          description: "This is the other2",
+          category: "Others"
+        }
+      ]
+    };
+  },
+
+  computed: {
+    filterList() {
+      return this.name === ""
+        ? this.list
+        : this.list.filter(
+            item =>
+              item.category === this.name ||
+              item.name === this.name ||
+              item.code === this.name ||
+              item.description === this.name
+          );
+    }
   }
 };
 </script>
+
+<style src="./home.css"></style>
